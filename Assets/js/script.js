8a0bf06076822b5eeee97278d9dccd7c89c38bb1
BHATTEN <byaccino@gmail.com>
2023-10-30 12:05:49 -0400

    add changes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,9 @@
 
 var startgame = document.querySelector ("#start-btn")
 var questionContainer = document.querySelector("#question-container")
+var scoreElement = document.querySelector("#score")
+var timeElement = document.querySelector("#time")
+
 var questions = [
     {
         question: "what is my favorite color?",
@@ -14,19 +17,24 @@ var questions = [
         correctAnswer: "Greece",
     }    
 ]
-var questionCount = 0
-
+var questionCount = 0;
+var score = 0;
+var time = 60;
 
 startgame.addEventListener('click',function(){
     startgame.classList.add('hide');
+    startTimer()
     displayQuestion()
 })
 
 function displayQuestion() {
+    //clear the question container
     questionContainer.innerHTML = '';
+    //create and display the question
     var question = document.createElement('h2')
     question.textContent = questions [questionCount].question
     questionContainer.append(question)
+    //create and display the answer choices as buttons 
     for (let i = 0; i < questions[questionCount].options.length; i++) {
         var button=document.createElement("button")
         button.textContent = questions [questionCount].options [i]
@@ -35,78 +43,43 @@ function displayQuestion() {
         questionContainer.append(button)
 
     }
+    //remove the 'hide' class to display the question and answer choices
     questionContainer.classList.remove('hide');
 
 }
-
-function checkAnswer(event) {
-    if (event.target.value === questions[questionCount].correctAnswer) {
-        // increment score value
-        
-    } else {
-        //time off of counter (answer wrong subtract time)
-    }
-    questionCount++
-    displayQuestion()
+var TimerInterval;
+function startTimer () {
+    TimerInterval = setInterval(function () {
+        timeElement.textContent = time;
+        time--;
+        if (time <0) {
+            clearInterval(TimerInterval)
+            timeElement.textContent = "0"
+        }
+    })
 }
 
-//case where no more questions
-//check if no more questions show final high score
-// if there are more questions display next question
+function checkAnswer(event) {
+    var selectedAnswer = event.target.value;
+    var currentQuestion = questions[questionCount]
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+        // increment score value
+        score +=1;
+        scoreElement.textContent = score; 
 
-
-
-// var timerEl = document.getElementById('countdown');
-// var mainEl = document.getElementById('main');
-
-// var message =
-//   'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
-// var words = message.split(' ');
-
-// // Timer that counts down from 5
-// function countdown() {
-//   var timeLeft = 5;
-
-//   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//   var timeInterval = setInterval(function () {
-//     // As long as the `timeLeft` is greater than 1
-//     if (timeLeft > 1) {
-//       // Set the `textContent` of `timerEl` to show the remaining seconds
-//       timerEl.textContent = timeLeft + ' seconds remaining';
-//       // Decrement `timeLeft` by 1
-//       timeLeft--;
-//     } else if (timeLeft === 1) {
-//       // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-//       timerEl.textContent = timeLeft + ' second remaining';
-//       timeLeft--;
-//     } else {
-//       // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-//       timerEl.textContent = '';
-//       // Use `clearInterval()` to stop the timer
-//       clearInterval(timeInterval);
-//       // Call the `displayMessage()` function
-//       displayMessage();
-//     }
-//   }, 1000);
-// }
-
-// // Displays the message one word at a time
-// function displayMessage() {
-//   var wordCount = 0;
-
-//   // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//   var msgInterval = setInterval(function () {
-//     // If there are no more words left in the message
-//     if (words[wordCount] === undefined) {
-//       // Use `clearInterval()` to stop the timer
-//       clearInterval(msgInterval);
-//     } else {
-//       // Display one word of the message
-//       mainEl.textContent = words[wordCount];
-//       wordCount++;
-//     }
-//   }, 1000);
-// }
-
-// countdown();
+    } else {
+        //time off of counter (answer wrong subtract time)
+        time -= 10;
+        if (time < 0) {
+            time = 0 // Ensure time doesn't go below 0
+        }
+    }
+    timeElement.textContent = time; 
+    questionCount++
+    if (questionCount < questions.length) {
+        displayQuestion();
+    } else {
+        questionContainer.innerHTML = "Quiz is over! You scored: " + score + " correct"
+    }
+}
 
